4f272dad89d3c6e07fce8cf99c226cc9f3e38c21
dongju93 <spdlqj011@gmail.com>
2024-09-07 18:15:32 +0900

    :card_file_box: update sqlalchemy model class for mapped with gql types

--- app/models.py
+++ app/models.py
@@ -1,255 +1,250 @@
 import base64
-from typing import Never, Optional
+from datetime import date, datetime
+from typing import Optional
 
 from sqlalchemy import (
     TIMESTAMP,
+    VARCHAR,
     Boolean,
-    Column,
     Date,
     ForeignKey,
     Integer,
     LargeBinary,
     Numeric,
-    String,
     Text,
 )
 from sqlalchemy.dialects.postgresql import ARRAY, TSVECTOR
 from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
 
 
-# Actor 테이블 모델 정의
-class Actor(Base):
+class ActorTable(Base):
     __tablename__ = "actor"
 
-    actor_id = Column(Integer, primary_key=True, index=True)
-    first_name = Column(String(45), nullable=False)
-    last_name = Column(String(45), nullable=False)
-    last_update = Column(TIMESTAMP, nullable=False)
+    # actor_id = Column(type_=Integer, primary_key=True, index=True)
+    actor_id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    first_name: Mapped[str] = mapped_column(VARCHAR(45), nullable=False)
+    last_name: Mapped[str] = mapped_column(VARCHAR(45), nullable=False)
+    last_update: Mapped[datetime] = mapped_column(TIMESTAMP, nullable=False)
 
 
-# Address 테이블 모델 정의
-class Address(Base):
+class AddressTable(Base):
     __tablename__ = "address"
 
-    address_id = Column(Integer, primary_key=True, index=True)
-    address = Column(String(50), nullable=False)
-    address2 = Column(String(50), nullable=True)
-    district = Column(String(20), nullable=False)
-    city_id = Column(Integer, ForeignKey("city.city_id"), nullable=False)
-    postal_code = Column(String(10), nullable=True)
-    phone = Column(String(20), nullable=False)
-    last_update = Column(TIMESTAMP, nullable=False)
+    address_id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    address: Mapped[str] = mapped_column(VARCHAR(50), nullable=False)
+    address2: Mapped[Optional[str]] = mapped_column(VARCHAR(50), nullable=True)
+    district: Mapped[str] = mapped_column(VARCHAR(20), nullable=False)
+    city_id: Mapped[int] = mapped_column(
+        ForeignKey("city.city_id"), nullable=False
+    )
+    postal_code: Mapped[Optional[str]] = mapped_column(
+        VARCHAR(10), nullable=True
+    )
+    phone: Mapped[str] = mapped_column(VARCHAR(20), nullable=False)
+    last_update: Mapped[datetime] = mapped_column(TIMESTAMP, nullable=False)
 
 
-# Category 테이블 모델 정의
-class Category(Base):
+class CategoryTable(Base):
     __tablename__ = "category"
 
-    category_id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(25), nullable=False)
-    last_update = Column(TIMESTAMP, nullable=False)
+    category_id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(VARCHAR(25), nullable=False)
+    last_update: Mapped[datetime] = mapped_column(TIMESTAMP, nullable=False)
 
 
-# City 테이블 모델 정의
-class City(Base):
+class CityTable(Base):
     __tablename__ = "city"
 
-    city_id = Column(Integer, primary_key=True, index=True)
-    city = Column(String(50), nullable=False)
-    country_id = Column(
-        Integer, ForeignKey("country.country_id"), nullable=False
+    city_id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    city: Mapped[str] = mapped_column(VARCHAR(50), nullable=False)
+    country_id: Mapped[int] = mapped_column(
+        ForeignKey("country.country_id"), nullable=False
     )
-    last_update = Column(TIMESTAMP, nullable=False)
+    last_update: Mapped[datetime] = mapped_column(TIMESTAMP, nullable=False)
 
 
-# Country 테이블 모델 정의
-class Country(Base):
+class CountryTable(Base):
     __tablename__ = "country"
 
-    country_id = Column(Integer, primary_key=True, index=True)
-    country = Column(String(50), nullable=False)
-    last_update = Column(TIMESTAMP, nullable=False)
+    country_id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    country: Mapped[str] = mapped_column(VARCHAR(50), nullable=False)
+    last_update: Mapped[datetime] = mapped_column(TIMESTAMP, nullable=False)
 
 
-# Customer 테이블 모델 정의
-class Customer(Base):
+class CustomerTable(Base):
     __tablename__ = "customer"
 
-    customer_id = Column(Integer, primary_key=True, index=True)
-    store_id = Column(Integer, nullable=False)
-    first_name = Column(String(45), nullable=False)
-    last_name = Column(String(45), nullable=False)
-    email = Column(String(50), nullable=True)
-    address_id = Column(
-        Integer, ForeignKey("address.address_id"), nullable=False
+    customer_id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    store_id: Mapped[int] = mapped_column(nullable=False)
+    first_name: Mapped[str] = mapped_column(VARCHAR(45), nullable=False)
+    last_name: Mapped[str] = mapped_column(VARCHAR(45), nullable=False)
+    email: Mapped[Optional[str]] = mapped_column(VARCHAR(50), nullable=True)
+    address_id: Mapped[int] = mapped_column(
+        ForeignKey("address.address_id"), nullable=False
     )
-    activebool = Column(Boolean, nullable=False, default=True)
-    create_date = Column(Date, nullable=False)
-    last_update = Column(TIMESTAMP, nullable=True)
-    active = Column(Integer, nullable=True)
+    activebool: Mapped[bool] = mapped_column(
+        Boolean, nullable=False, default=True
+    )
+    create_date: Mapped[date] = mapped_column(Date, nullable=False)
+    last_update: Mapped[Optional[datetime]] = mapped_column(
+        TIMESTAMP, nullable=True
+    )
+    active: Mapped[Optional[int]] = mapped_column(Integer, nullable=True)
 
 
-# Film 테이블 모델 정의
-class Film(Base):
+class FilmTable(Base):
     __tablename__ = "film"
 
-    film_id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(255), nullable=False)
-    description = Column(Text, nullable=True)
-    release_year = Column(Integer, nullable=True)
-    language_id = Column(
-        Integer, ForeignKey("language.language_id"), nullable=False
+    film_id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(VARCHAR(255), nullable=False)
+    description: Mapped[Optional[str]] = mapped_column(Text, nullable=True)
+    release_year: Mapped[Optional[int]] = mapped_column(Integer, nullable=True)
+    language_id: Mapped[int] = mapped_column(
+        ForeignKey("language.language_id"), nullable=False
     )
-    rental_duration = Column(Integer, nullable=False, default=3)
-    rental_rate = Column(Numeric(4, 2), nullable=False, default=4.99)
-    length = Column(Integer, nullable=True)
-    replacement_cost = Column(Numeric(5, 2), nullable=False, default=19.99)
-    rating = Column(String, nullable=True)
-    last_update = Column(TIMESTAMP, nullable=False)
-    special_features: Column[Never] = Column(ARRAY(Text), nullable=True)
+    rental_duration: Mapped[int] = mapped_column(
+        Integer, nullable=False, default=3
+    )
+    rental_rate: Mapped[float] = mapped_column(
+        Numeric(4, 2), nullable=False, default=4.99
+    )
+    length: Mapped[Optional[int]] = mapped_column(Integer, nullable=True)
+    replacement_cost: Mapped[float] = mapped_column(
+        Numeric(5, 2), nullable=False, default=19.99
+    )
+    rating: Mapped[Optional[str]] = mapped_column(VARCHAR, nullable=True)
+    last_update: Mapped[datetime] = mapped_column(TIMESTAMP, nullable=False)
+    special_features: Mapped[Optional[list[str]]] = mapped_column(
+        ARRAY(Text), nullable=True
+    )
+    fulltext: Mapped[str] = mapped_column(TSVECTOR, nullable=False)
 
-    fulltext = Column(TSVECTOR, nullable=False)
 
-
-# FilmActor 테이블 모델 정의
-class FilmActor(Base):
+class FilmActorTable(Base):
     __tablename__ = "film_actor"
 
-    actor_id = Column(
-        Integer,
+    actor_id: Mapped[int] = mapped_column(
         ForeignKey("actor.actor_id", ondelete="RESTRICT", onupdate="CASCADE"),
         primary_key=True,
     )
-    film_id = Column(
-        Integer,
+    film_id: Mapped[int] = mapped_column(
         ForeignKey("film.film_id", ondelete="RESTRICT", onupdate="CASCADE"),
         primary_key=True,
     )
-    last_update = Column(TIMESTAMP, nullable=False)
+    last_update: Mapped[datetime] = mapped_column(TIMESTAMP, nullable=False)
 
 
-# FilmCategory 테이블 모델 정의
-class FilmCategory(Base):
+class FilmCategoryTable(Base):
     __tablename__ = "film_category"
 
-    film_id = Column(
-        Integer,
+    film_id: Mapped[int] = mapped_column(
         ForeignKey("film.film_id", ondelete="RESTRICT", onupdate="CASCADE"),
         primary_key=True,
     )
-    category_id = Column(
-        Integer,
+    category_id: Mapped[int] = mapped_column(
         ForeignKey(
             "category.category_id", ondelete="RESTRICT", onupdate="CASCADE"
         ),
         primary_key=True,
     )
-    last_update = Column(TIMESTAMP, nullable=False)
+    last_update: Mapped[datetime] = mapped_column(TIMESTAMP, nullable=False)
 
 
-# Inventory 테이블 모델 정의
-class Inventory(Base):
+class InventoryTable(Base):
     __tablename__ = "inventory"
 
-    inventory_id = Column(Integer, primary_key=True, index=True)
-    film_id = Column(
-        Integer,
+    inventory_id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    film_id: Mapped[int] = mapped_column(
         ForeignKey("film.film_id", ondelete="RESTRICT", onupdate="CASCADE"),
         nullable=False,
     )
-    store_id = Column(Integer, nullable=False)
-    last_update = Column(TIMESTAMP, nullable=False)
+    store_id: Mapped[int] = mapped_column(nullable=False)
+    last_update: Mapped[datetime] = mapped_column(TIMESTAMP, nullable=False)
 
 
-# Language 테이블 모델 정의
-class Language(Base):
+class LanguageTable(Base):
     __tablename__ = "language"
 
-    language_id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(20), nullable=False)
-    last_update = Column(TIMESTAMP, nullable=False)
+    language_id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(VARCHAR(20), nullable=False)
+    last_update: Mapped[datetime] = mapped_column(TIMESTAMP, nullable=False)
 
 
-# Payment 테이블 모델 정의
-class Payment(Base):
+class PaymentTable(Base):
     __tablename__ = "payment"
 
-    payment_id = Column(Integer, primary_key=True, index=True)
-    customer_id = Column(
-        Integer,
+    payment_id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    customer_id: Mapped[int] = mapped_column(
         ForeignKey(
             "customer.customer_id", ondelete="RESTRICT", onupdate="CASCADE"
         ),
         nullable=False,
     )
-    staff_id = Column(
-        Integer,
+    staff_id: Mapped[int] = mapped_column(
         ForeignKey("staff.staff_id", ondelete="RESTRICT", onupdate="CASCADE"),
         nullable=False,
     )
-    rental_id = Column(
-        Integer,
+    rental_id: Mapped[int] = mapped_column(
         ForeignKey(
             "rental.rental_id", ondelete="SET NULL", onupdate="CASCADE"
         ),
         nullable=False,
     )
-    amount = Column(Numeric(5, 2), nullable=False)
-    payment_date = Column(TIMESTAMP, nullable=False)
+    amount: Mapped[float] = mapped_column(Numeric(5, 2), nullable=False)
+    payment_date: Mapped[datetime] = mapped_column(TIMESTAMP, nullable=False)
 
 
-# Rental 테이블 모델 정의
-class Rental(Base):
+class RentalTable(Base):
     __tablename__ = "rental"
 
-    rental_id = Column(Integer, primary_key=True, index=True)
-    rental_date = Column(TIMESTAMP, nullable=False)
-    inventory_id = Column(
-        Integer,
+    rental_id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    rental_date: Mapped[datetime] = mapped_column(TIMESTAMP, nullable=False)
+    inventory_id: Mapped[int] = mapped_column(
         ForeignKey(
             "inventory.inventory_id", ondelete="RESTRICT", onupdate="CASCADE"
         ),
         nullable=False,
     )
-    customer_id = Column(
-        Integer,
+    customer_id: Mapped[int] = mapped_column(
         ForeignKey(
             "customer.customer_id", ondelete="RESTRICT", onupdate="CASCADE"
         ),
         nullable=False,
     )
-    return_date = Column(TIMESTAMP, nullable=True)
-    staff_id = Column(
-        Integer,
+    return_date: Mapped[Optional[datetime]] = mapped_column(
+        TIMESTAMP, nullable=True
+    )
+    staff_id: Mapped[int] = mapped_column(
         ForeignKey("staff.staff_id", ondelete="RESTRICT", onupdate="CASCADE"),
         nullable=False,
     )
-    last_update = Column(TIMESTAMP, nullable=False)
+    last_update: Mapped[datetime] = mapped_column(TIMESTAMP, nullable=False)
 
 
-# Staff 테이블 모델 정의
-class Staff(Base):
+class StaffTable(Base):
     __tablename__ = "staff"
 
-    staff_id = Column(Integer, primary_key=True, index=True)
-    first_name = Column(String(45), nullable=False)
-    last_name = Column(String(45), nullable=False)
-    address_id = Column(
-        Integer,
+    staff_id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    first_name: Mapped[str] = mapped_column(VARCHAR(45), nullable=False)
+    last_name: Mapped[str] = mapped_column(VARCHAR(45), nullable=False)
+    address_id: Mapped[int] = mapped_column(
         ForeignKey(
             "address.address_id", ondelete="RESTRICT", onupdate="CASCADE"
         ),
         nullable=False,
     )
-    email = Column(String(50), nullable=True)
-    store_id = Column(Integer, nullable=False)
-    active = Column(Boolean, nullable=False, default=True)
-    username = Column(String(16), nullable=False)
-    password = Column(String(40), nullable=True)
-    last_update = Column(TIMESTAMP, nullable=False)
-    picture = Column(LargeBinary, nullable=True)
+    email: Mapped[Optional[str]] = mapped_column(VARCHAR(50), nullable=True)
+    store_id: Mapped[int] = mapped_column(nullable=False)
+    active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
+    username: Mapped[str] = mapped_column(VARCHAR(16), nullable=False)
+    password: Mapped[Optional[str]] = mapped_column(VARCHAR(40), nullable=True)
+    last_update: Mapped[datetime] = mapped_column(TIMESTAMP, nullable=False)
+    picture: Mapped[Optional[bytes]] = mapped_column(
+        LargeBinary, nullable=True
+    )
 
     # picture 필드를 Base64로 인코딩된 문자열로 반환
     @hybrid_property
@@ -260,21 +255,18 @@ class Staff(Base):
         return None
 
 
-# Store 테이블 모델 정의
-class Store(Base):
+class StoreTable(Base):
     __tablename__ = "store"
 
-    store_id = Column(Integer, primary_key=True, index=True)
-    manager_staff_id = Column(
-        Integer,
+    store_id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    manager_staff_id: Mapped[int] = mapped_column(
         ForeignKey("staff.staff_id", ondelete="RESTRICT", onupdate="CASCADE"),
         nullable=False,
     )
-    address_id = Column(
-        Integer,
+    address_id: Mapped[int] = mapped_column(
         ForeignKey(
             "address.address_id", ondelete="RESTRICT", onupdate="CASCADE"
         ),
         nullable=False,
     )
-    last_update = Column(TIMESTAMP, nullable=False)
+    last_update: Mapped[datetime] = mapped_column(TIMESTAMP, nullable=False)
